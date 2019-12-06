341bc48551601e3e8400ae97317174ee791c444e
eduardo jimenez <eduardo.jimenez@kriosoft.com>
2019-12-06 14:31:25 -0600

    cambio

---
+++ AdminVivienda/Models/EstadoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdminVivienda.Models
+{
+    public class EstadoModel
+    {
+        public int id_Estado {get;set;}
+        public string Estado { get; set; }
+    }
+}
---
+++ AdminVivienda/Models/Vivienda/ViviendaIndexModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdminVivienda.Models.Vivienda
+{
+    public class ViviendaIndexModel
+    {
+        public List<EstadoModel> Estados { get; set; }
+        public List<ViviendaGridModel> Viviendas { get; set; }
+    }
+}
