9d613b994d3539016deafbe303206853eb89f979
eduardo <ejjimenezgarcia@gmail.com>
2019-12-10 15:29:21 -0600

    a

---
+++ AdminVivienda/Models/Catalogos/General/Nivel1RecursosModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdminVivienda.Models.Catalogos.General
+{
+    public class Nivel1RecursosModel
+    {
+        public Nivel1RecursosModel() {
+            datos = new List<Nivel1Model>();
+        }
+         public string tituloIndex { get; set; }
+        public string tituloAgregar { get; set; }
+        public string tituloEditar { get; set; }
+        public string campo1 { get; set; }
+        public int maxValue { get; set; }
+        public string estatus { get; set; }
+        List<Nivel1Model> datos { get; set; }
+    }
+}
