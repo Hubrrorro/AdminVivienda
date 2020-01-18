3c19adfb7ea2fc1c10a2367a06d5b88bc5487120
eduardo jimenez <eduardo.jimenez@kriosoft.com>
2020-01-17 19:54:50 -0600

    cambios

---
+++ Version2.0/AdminVivienda2/AdminVivienda2/Controllers/Administracion/ContratoAdminController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminVivienda2.Controllers.Administracion
+{
+    public class ContratoAdminController : Controller
+    {
+        //IGeneral<CAT_CONDOMINIOS, CondominioModel> condominioBl;
+        //public CondominioController()
+        //{
+        //    condominioBl = new CondominioBL();
+        //}
+        //// GET: Condominio
+        //public ActionResult Index()
+        //{
+        //    ViewBag.Title = "Buscar condominio";
+        //    return View();
+        //}
+        //public ActionResult Details(int id)
+        //{
+        //    ViewBag.Accion = "Actualizar";
+        //    ViewBag.Title = "Editar condominio";
+        //    var resul = condominioBl.ConsultarId(id);
+        //    return View("~/Views/Condominio/Condominio.cshtml", (DAL.CAT_CONDOMINIOS)resul.datos);
+        //}
+        //public ActionResult Create()
+        //{
+        //    ViewBag.Accion = "Agregar";
+        //    ViewBag.Title = "Agregar condominio";
+        //    CAT_CONDOMINIOS condominio = new CAT_CONDOMINIOS();
+        //    return View("~/Views/Condominio/Condominio.cshtml", condominio);
+        //}
+        //public PartialViewResult Grid(CondominioModel modelo)
+        //{
+        //    var resul = condominioBl.Consultar(modelo);
+        //    return PartialView("~/Views/Condominio/Grid.cshtml", (List<DAL.CAT_CONDOMINIOS>)resul.datos);
+        //}
+        //[HttpPost]
+        //public ActionResult Create(CAT_CONDOMINIOS modelo)
+        //{
+        //    var resul = condominioBl.Agregar(modelo);
+        //    return Json(resul);
+        //}
+        //[HttpPost]
+        //public ActionResult Edit(CAT_CONDOMINIOS modelo)
+        //{
+        //    var resul = condominioBl.Actualizar(modelo);
+        //    return Json(resul);
+        //}
+        //[HttpPost]
+        //public ActionResult Select()
+        //{
+        //    var resul = condominioBl.Select();
+        //    return Json(resul);
+        //}
+    }
+}
---
+++ Version2.0/AdminVivienda2/AdminVivienda2/Controllers/Catalogos/ServiciosController.cs
@@ -0,0 +1,28 @@
+﻿using AdminVivienda2.Models;
+
+namespace AdminVivienda2.Controllers.Catalogos
+{
+    public class ServiciosController : Nivel1Campo1Controller
+    {
+        // GET: TipoVivienda
+        private static TablaNivel1 _tablaNivel1 = new TablaNivel1()
+        {
+            descripcion = "Servicios",
+            idNombre = "Id_Servicio",
+            nombreTabla = "TBL_SERVICIOS"
+        };
+        private static Nivel1Campo1Model _nivel1 = new Nivel1Campo1Model()
+        {
+            campo1 = "Servicios",
+            maxValue = 100,
+            estatus = "Estatus",
+            tituloAgregar = "Agregar servicios",
+            tituloEditar = "Editar servicios",
+            tituloIndex = "Buscar servicios",
+            tabla = _tablaNivel1
+        };
+        public ServiciosController() : base(_nivel1)
+        {
+        }
+    }
+}
