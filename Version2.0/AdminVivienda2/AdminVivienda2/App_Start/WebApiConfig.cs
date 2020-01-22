07a9e9440411ac654ac8eb48f6fbe109462872b4
eduardo jimenez <eduardo.jimenez@kriosoft.com>
2020-01-22 17:28:23 -0600

    Seguridad

--- Version2.0/AdminVivienda2/AdminVivienda2/App_Start/WebApiConfig.cs
+++ Version2.0/AdminVivienda2/AdminVivienda2/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdminVivienda2.Controllers.Login;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -12,7 +13,16 @@ namespace AdminVivienda2
             // Configuración y servicios de API web
 
             // Rutas de API web
+            //config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
             config.MapHttpAttributeRoutes();
+
+            config.MessageHandlers.Add(new TokenValidationHandler());
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
---
+++ Version2.0/AdminVivienda2/AdminVivienda2/Controllers/Login/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using AdminVivienda2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+namespace AdminVivienda2.Controllers.Login
+{
+    /// <summary>
+    /// login controller class for authenticate users
+    /// </summary>
+    [AllowAnonymous]
+    //[RoutePrefix("api/login")]
+    public class LoginController : ApiController
+    {
+        [HttpGet]
+        [Route("api/login/echoping")]
+        public IHttpActionResult EchoPing()
+        {
+            return Ok(true);
+        }
+
+        [HttpGet]
+        [Route("api/login/echouser")]
+        public IHttpActionResult EchoUser()
+        {
+            var identity = Thread.CurrentPrincipal.Identity;
+            return Ok($" IPrincipal-user: {identity.Name} - IsAuthenticated: {identity.IsAuthenticated}");
+        }
+        [HttpPost]
+        [Route("api/login")]
+        public IHttpActionResult Post()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        [Route("api/login/authenticate")]
+        public IHttpActionResult Authenticate([FromBody]LoginRequest login)
+        {
+            if (login == null)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            //TODO: Validate credentials Correctly, this code is only for demo !!
+            bool isCredentialValid = (login.Password == "123456");
+            if (isCredentialValid)
+            {
+                var token = TokenGenerator.GenerateTokenJwt(login.Username);
+                return Ok(token);
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
